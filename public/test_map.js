312545b14b74d925a0394e5af8320ec401b76703
PaulCochet <paul.cochet0809@gmail.com>
2026-03-25 16:10:43 +0100

    Chengement map

--- public/test_map.js
+++ public/test_map.js
@@ -2,7 +2,7 @@ let mapModel;
 
 function preload() {
   console.log("Starting preload...");
-  mapModel = loadModel('Img/workshop_map.glb', true, () => {
+  mapModel = loadModel('Img/Workshop_mapV4.glb', true, () => {
     console.log("Model loaded successfully!");
   }, (err) => {
     console.error("Error loading model:", err);
@@ -17,12 +17,12 @@ function setup() {
 
 function draw() {
   background(15, 15, 25);
-  
+
   orbitControl(); // allow mouse drag to rotate
-  
+
   ambientLight(150, 150, 150);
   directionalLight(255, 255, 255, 0, 1, -1);
-  
+
   push();
   // Rotate to match typical p5 orientations
   rotateX(PI); // Sometimes models are upside down
